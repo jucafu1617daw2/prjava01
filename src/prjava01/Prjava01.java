3eb8ba99f98356171317a4b2b8b2ea3d11700ccc
Excombatient <shoot1994@hotmail.com>
2017-01-18 21:06:35 +0100

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -14,6 +14,7 @@ public class Prjava01{
      * @param args the command line arguments
      */
 public static void main(String[] args) throws IOException {
+    
     File f = new File("fitxer.html");
     try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
     bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
